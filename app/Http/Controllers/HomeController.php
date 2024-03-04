0ec4ec888dfdace8628601448015f88e916163d5
AlbertWebs <albertmuhatia@gmail.com>
2024-03-04 10:27:36 +0300

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     }
 
     public function excellence(){
-        $title = " Pr And Communication Services ";
+        $title = " PR And Communication Services ";
         return view('front.excellence', compact('title'));
     }
 
